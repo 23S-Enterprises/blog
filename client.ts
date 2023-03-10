fd918be779368817bfbcf43f9a069d6132c2ac53
23S Enterprises <admin@23s.enterprises>
2023-03-09 22:36:24 -0700

    Done

--- client.ts
+++ client.ts
@@ -4,5 +4,4 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: '2wsm2x5t', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
-})
+  useCdn: true // `false` if you want to ensure fresh da
